e25981b3f3706c57145aae7ec2b3d8cb117885b3
ae2rs <lucasdc@amo.co>
2025-12-23 18:50:10 +0100

    added image dir servce

--- bin/main.rs
+++ bin/main.rs
@@ -9,7 +9,8 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(pages::index))
-        .nest_service("/style", ServeDir::new("public/style"));
+        .nest_service("/style", ServeDir::new("public/style"))
+        .nest_service("/img", ServeDir::new("public/img"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
