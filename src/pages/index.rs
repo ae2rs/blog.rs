ad0d2bf8e29a3295813c72866c1cd9984125aaa8
ae2rs <lucasdc@amo.co>
2025-12-24 22:47:24 +0100

    homepage

--- src/pages/index.rs
+++ src/pages/index.rs
@@ -4,18 +4,26 @@ use maud::{Markup, html};
 pub async fn page() -> Markup {
     let content = html! {
         h1 { "Lucas' Hut" }
-        div class="flex flex-col items-start gap-4 sm:flex-row sm:items-center my-4" {
-            img
-                src="/img/avatar.png"
+        div class="flex flex-col items-start gap-6 sm:flex-row sm:items-center my-4" {
+            img src="/img/avatar.png"
                 alt="Portrait of Lucas de Castro"
-                class="h-24 w-24 shrink-0 rounded-full object-cover";
-            p class="text-base leading-relaxed" {
-                "Hey, I'm Lucas de Castro. I'm a backend software engineer, and this open source blog is where I (irregularly) post about pretty much anything I find interesting."
+                class="size-32 shrink-0 rounded-full border-4 border-white/20 object-cover";
+            p class="m-0 text-base leading-relaxed" {
+                "Hey, I'm Lucas de Castro. I'm a backend software engineer, and this "
+                a href="https://github.com/ae2rs/blog.rs" { "open source" }
+                " blog is where I (irregularly) post about pretty much anything I find interesting."
+                br;
+                a class="mt-2 inline-block" href="/about" { "Read more ››" }
             }
         }
 
-        p {
-            a href="#" { "Read the first post" }
+        section class="mt-8" {
+            h2 class="text-xl font-semibold" { "Latest posts" }
+            ul class="mt-4 space-y-2" {
+                li {
+                    a href="#" { "Read the first post" }
+                }
+            }
         }
     };
 
